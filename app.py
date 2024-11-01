5a820cc190b9aca9b6ff14b0741d6210c0d285a5
Kwrokk <160144000+Kwrokk@users.noreply.github.com>
2024-11-01 09:37:44 -0300

    primeira versão

---
+++ app.py
@@ -0,0 +1,68 @@
+import os
+
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+
+from flask_sqlalchemy import SQLAlchemy
+
+project_dir = os.path.dirname(os.path.abspath(__file__));
+
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"));
+
+app = Flask(__name__);
+
+app.config["SQLALCHEMY_DATABASE-URI"] = database_file;
+
+db = SQLAlchemy(app);
+
+class Book(db.Model):
+    title = db.Column(db.String(80), unique = True, nullable = False, primary_key = True);
+    autor = db.Column(db.String(80), nullable = True);
+    genero = db.Column(db.String(80), nullable = True);
+    num_pag = db.Column(db.Integer, nullable = True)
+
+    def __repr__(self):
+        return "<Title: {}>".format(self.title);
+
+
+@app.route('/', methods=["GET","POST"])
+def home():
+    books = None 
+    if request.form:
+        try:
+            book = Book(title = request.form.get("title"))
+            db.session.add(book)
+            db.session.commit()
+        except Exception as e:
+            print("Falha ao adicionar livro")
+            print(e)
+
+    books = Book.query.all()
+    return render_template("index.html", books=books)
+
+
+@app.route("/update",methods=["POST"])
+def update():
+    try:                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+        newtitle = request.form.get("newtitle")
+        oldtitle = request.form.get("oldtitle")
+        book = Book.query.filter_by(title=oldtitle).first()
+        book.title = newtitle
+        db.session.commit()
+    except Exception as e:
+        print("Não dá pra fazer update")
+        print(e)
+    return redirect("/")
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    title = request.form.get("title")
+    book = Book.query.filter_by(title=title).first()
+    db.session.delete(book)
+    db.session.commit()
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
